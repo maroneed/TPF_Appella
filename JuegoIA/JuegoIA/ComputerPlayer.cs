8d61d7508d7931c38adbfad383172b4e26a59056
maroneed <gabriell.appella@gmail.com>
2020-06-25 18:04:25 -0300

    clase movimiento

--- JuegoIA/JuegoIA/ComputerPlayer.cs
+++ JuegoIA/JuegoIA/ComputerPlayer.cs
@@ -1,4 +1,5 @@
 ﻿
+using JuegoIA;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,14 +8,19 @@ namespace juegoIA
 {
 	public class ComputerPlayer: Jugador
 	{
-		
+		private ArbolGeneral<Movimiento> arbol;
 		public ComputerPlayer()
 		{
 		}
 		
 		public override void  incializar(List<int> cartasPropias, List<int> cartasOponente, int limite)
 		{
-			//Implementar
+			Movimiento comienzo = new Movimiento(-1,limite,0,true);
+			arbol = new ArbolGeneral<Movimiento>(comienzo);
+
+			generarArbol(this.arbol, cartasPropias, cartasOponente, limite);
+			Console.WriteLine(this.arbol.getHijos().count);
+
 		}
 		
 		
---
+++ JuegoIA/JuegoIA/Movimiento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JuegoIA
+{
+    public class Movimiento
+    {
+        private int carta { get; set; }
+        private int limiteActual { get; set; }
+        private int ganadas { get; set; }
+        private bool esAi { get; set; }
+
+        public Movimiento(int carta, int limiteActual, int ganadas, bool esAi)
+        {
+            this.carta = carta;
+            this.limiteActual = limiteActual;
+            this.ganadas = ganadas;
+            this.esAi = esAi;
+        }
+    }
+}
